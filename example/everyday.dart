aac8856c6edab2cb8e8de939dfb190d76b9337cb
Leo Letto <leandroletto@gmail.com>
2020-11-05 16:21:02 +0100

    Update everyday.dart

--- example/everyday.dart
+++ example/everyday.dart
@@ -3,7 +3,7 @@ import 'package:cron/cron.dart';
 void main() async {
   final cron = Cron();
 
-  /// Schedule that will thell the cron to run every day from Monday through Friday at 7AM
+  /// Schedule that will tell the cron to run every day from Monday through Friday at 7AM
   /// You could also use the syntax `0 0 7 * * 1-5 *`
   final task = cron.schedule(Schedule.parse('0 0 7 ? * MON-FRI *'), () {
     print('Runned at ${DateTime.now()}');
