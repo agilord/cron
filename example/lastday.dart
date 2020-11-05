37a3d7fadb8f02033aaf3ea1749a99d93f1f907d
Leo Letto <leandroletto@gmail.com>
2020-11-05 16:21:17 +0100

    Update lastday.dart

--- example/lastday.dart
+++ example/lastday.dart
@@ -3,7 +3,7 @@ import 'package:cron/cron.dart';
 void main() async {
   final cron = Cron();
 
-  /// Schedule that will thell the cron to run on Friday of each month at 7AM
+  /// Schedule that will tell the cron to run on Friday of each month at 7AM
   /// You could also use the syntax `0 0 7 * * 5L *`
   final task = cron.schedule(Schedule.parse('0 0 7 ? * 5L *'), () {
     print('Runned at ${DateTime.now()}');
